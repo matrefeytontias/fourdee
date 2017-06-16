9816110114f55ed06b89d63319af37c031904b8a
matrefeytontias <mattias@refeyton.fr>
2017-06-16 16:39:21 +0200

    Added Object4D.buildMatrix5

--- fourdee.js
+++ fourdee.js
@@ -12,6 +12,23 @@ function Object4D()
   this.projection = undefined; // use your own 3D projection
 }
 
+Object4D.prototype.buildMatrix5 = function ()
+{
+  var mat = new Matrix5();
+  
+  mat.scale(this.scale, this.scale, this.scale, this.scale);
+  mat.translate(this.position.x, this.position.y, this.position.z, this.position.w);
+  // Handle rotations following the order described in this.rotation.order
+  for(var i = 0; i + 1 < this.rotation.order.length; i += 2)
+  {
+    var rotationPlane = this.rotation.order.slice(i, i + 2);
+    var theta = this.rotation[rotationPlane.toLowerCase()];
+    if(theta != 0)
+      mat.rotate(rotationPlane, theta);
+  }
+  return mat;
+}
+
 /**************
  * Proj4D API *
  **************/
