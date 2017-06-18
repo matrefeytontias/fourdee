cf4b1e8113671a8766778371a71574f2b49abc45
matrefeytontias <mattias@refeyton.fr>
2017-06-18 02:29:25 +0200

    Added stereographical projection ; change projections on mouse click

--- fourdee.js
+++ fourdee.js
@@ -40,15 +40,45 @@ function Proj4D() { }
 // Vector4 v
 Proj4D.prototype.project = function (v) { }
 
+// 
 // Linear algebra projection : (x, y, z, w) -> (x, y, z, 0)
+// 
 function OrthoProj()
 {
   Proj4D.call(this);
 }
 
 OrthoProj.prototype = new Proj4D();
-
 OrthoProj.prototype.project = function (v)
 {
   return new THREE.Vector3(v.x, v.y, v.z);
+}
+
+// 
+// Stereographical projection : uses a 3-sphere to project the 4D point onto the hyperplane w = 0
+// 
+// THREE.Vector4 center, float radius, THREE.Vector4 planeNormal, THREE.Vector4 planePoint
+function StereoProj(center, radius)
+{
+  Proj4D.call(this);
+  this.sphereCenter = center;
+  this.sphereRadius = radius;
+  // The pole is the sphere's furthest point from the hyperplane w = 0
+  this.spherePole = (new THREE.Vector4(0, 0, 0, radius * ((center.w > 0) * 2 - 1))).add(center);
+}
+
+StereoProj.prototype = new Proj4D();
+StereoProj.prototype.project = function (v)
+{
+  // Projection of the point on the 3-sphere
+  var sphereProj = v.clone().sub(this.sphereCenter);
+  sphereProj.multiplyScalar(this.sphereRadius / sphereProj.length()).add(this.sphereCenter);
+  
+  var u = sphereProj.clone().sub(this.spherePole).normalize();
+  if(u.w != 0)
+  {
+    var t = -this.spherePole.w / u.w;
+    u = u.multiplyScalar(t).add(this.spherePole);
+  }
+  return new THREE.Vector3(u.x, u.y, u.z);
 }
--- main.js
+++ main.js
@@ -5,7 +5,8 @@ const D4_gameWidth = D4_container.offsetWidth;
 const D4_gameHeight = D4_container.offsetHeight;
 const D4_aspectRatio = D4_gameWidth / D4_gameHeight;
 const D4_scene = new THREE.Scene();
-const D4_space = new Space4D(new OrthoProj());
+// const D4_space = new Space4D(new OrthoProj());
+const D4_space = new Space4D(new StereoProj(new THREE.Vector4(0, 0, 0, 5), 3));
 
 if(!D4_PERSPECTIVE)
 {
@@ -20,13 +21,22 @@ var cube;
 
 window.addEventListener("load", main);
 
+const orthoProj = new OrthoProj();
+const stereoProj = new StereoProj(new THREE.Vector4(0, 0, 0, 5), 3);
+window.addEventListener("mouseup", function (e)
+{
+  D4_space.projector = D4_space.projector === orthoProj ? stereoProj : orthoProj;
+});
+
 function main()
 {
   D4_renderer.setSize(D4_gameWidth, D4_gameHeight);
   D4_container.appendChild(D4_renderer.domElement);
 
-  var geometry = new BoxGeometry4D(1, 1, 1, 1);
-  cube = new Mesh4D(geometry, new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide, wireframe: true }));
+  // var geometry = new BoxGeometry4D(1, 1, 1, 1);
+  var geometry = new BoxLinesGeometry4D(1, 1, 1, 1);
+  // cube = new Mesh4D(geometry, new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide }));
+  cube = new LineSegments4D(geometry, new THREE.LineBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide, wireframe: true }));
   D4_scene.add(cube.projection);
   D4_space.add(cube);
   D4_camera.position.z = 5;
@@ -42,7 +52,7 @@ function render()
 {
   requestAnimationFrame(render);
   cube.rotation.xz += 0.01;
-  D4_space.rotation.xw += 0.01;
+  D4_space.rotation.yw += 0.01;
   D4_space.project();
   D4_renderer.render(D4_scene, D4_camera);
 }
