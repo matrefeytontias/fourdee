ae463f60a3bd81888cab6612bcae1bf7fbf6ec6c
Mattias Refeyton <mattias.refeyton@telecom-paristech.fr>
2017-06-23 15:59:42 +0200

    Moved vertices, faces, edges and face groups definitions to respective geometry files

--- geometries/Box3DGeometry4D.js
+++ geometries/Box3DGeometry4D.js
@@ -3,12 +3,12 @@
 function Box3DGeometry4D(dimension, width, height, depth)
 {
   Geometry4D.call(this);
-  
+
   var d1 = dimension.charAt(0), d2 = dimension.charAt(1), d3 = dimension.charAt(2);
-  
-   
+
+
   var v = new THREE.Vector4(0, 0, 0, 0);
-   
+
   for(var i = -1/2; i < 1; i++)
   {
     v[d1] = i * width
@@ -18,20 +18,21 @@ function Box3DGeometry4D(dimension, width, height, depth)
       for(var k = -1/2; k < 1; k++)
       {
         v[d3] = k * depth;
-        this.vertices4D.push(v.clone()); 
+        this.vertices4D.push(v.clone());
       }
     }
   }
-  
+
   for(var i = 0; i < cubeFaces.length; i++)
     this.faces.push(cubeFaces[i].clone());;
-  
+
+  this.faceGroups = [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9], [10, 11]];
 }
 
 Box3DGeometry4D.prototype = new Geometry4D();
 Box3DGeometry4D.prototype.constructor = Box3DGeometry4D;
 
-/* cube vertices 
+/* cube vertices
 - - -
 - - +
 - + -
@@ -43,17 +44,17 @@ Box3DGeometry4D.prototype.constructor = Box3DGeometry4D;
 + + +
 */
 
-const cubeFaces = [ 
-  new THREE.Face3(5, 7, 3), 
-  new THREE.Face3(3, 1, 5), 
+const cubeFaces = [
+  new THREE.Face3(5, 7, 3),
+  new THREE.Face3(3, 1, 5),
   new THREE.Face3(4, 6, 7),
-  new THREE.Face3(7, 5, 4), 
+  new THREE.Face3(7, 5, 4),
   new THREE.Face3(7, 6, 2),
-  new THREE.Face3(2, 3, 7), 
+  new THREE.Face3(2, 3, 7),
   new THREE.Face3(1, 3, 2),
-  new THREE.Face3(2, 0, 1), 
+  new THREE.Face3(2, 0, 1),
   new THREE.Face3(0, 4, 5),
-  new THREE.Face3(5, 1, 0), 
+  new THREE.Face3(5, 1, 0),
   new THREE.Face3(2, 6, 4),
-  new THREE.Face3(4, 0, 2) 
+  new THREE.Face3(4, 0, 2)
 ];
