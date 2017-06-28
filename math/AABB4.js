0cc738a1de6b1f2a05c36b2044aab6348be08c12
matrefeytontias <mattias@refeyton.fr>
2017-06-28 15:58:07 +0200

    Changed conditions on AABB intersection

--- math/AABB4.js
+++ math/AABB4.js
@@ -17,5 +17,5 @@ AABB4.prototype.intersects = function(other)
   var cheight = Math.max(Math.abs(this.p.y + this.d.y / 2 - (other.p.y - other.d.y / 2)), Math.abs(other.p.y + other.d.y / 2 - (this.p.y - this.d.y / 2)));
   var cdepth = Math.max(Math.abs(this.p.z + this.d.z / 2 - (other.p.z - other.d.z / 2)), Math.abs(other.p.z + other.d.z / 2 - (this.p.z - this.d.z / 2)));
   var cduth = Math.max(Math.abs(this.p.w + this.d.w / 2 - (other.p.w - other.d.w / 2)), Math.abs(other.p.w + other.d.w / 2 - (this.p.w - this.d.w / 2)));
-  return cwidth < this.d.x + other.d.x && cheight < this.d.y + other.d.y && cdepth < this.d.z + other.d.z && cduth < this.d.w + other.d.w;
+  return cwidth <= this.d.x + other.d.x && cheight <= this.d.y + other.d.y && cdepth <= this.d.z + other.d.z && cduth <= this.d.w + other.d.w;
 }
