ca90272f26cb558db63bf4b43f205a2be21e513b
Arthur Lacote <arthur.lacote1@free.fr>
2017-06-28 09:41:07 +0000

    Added the level selection menu

--- geometries/PentacoreGeometry.js
+++ geometries/PentacoreGeometry.js
@@ -40,6 +40,10 @@ function PentachoreGeometry4D(scale = 1, width=null, height=null, depth=null, du
     [0, 1, 2, 3],
     [4, 5, 6, 7, 8, 9]
   ]
+  
+  this.edges = [
+    0, 1, 0, 2, 0, 3, 0, 4, 1, 2, 1, 3, 1, 4, 2, 3, 2, 4, 3, 4
+  ]
 }
 
 PentachoreGeometry4D.prototype = new Geometry4D();
